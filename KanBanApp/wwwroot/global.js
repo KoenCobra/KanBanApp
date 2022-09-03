22d014f152ef93d6a0bd17a3d23b2ce057af3b8c
Koen De Groot <koendegroot84@gmail.com>
2022-09-03 13:54:22 +0200

    added js dropdown

--- KanBanApp/wwwroot/global.js
+++ KanBanApp/wwwroot/global.js
@@ -1,1 +1,16 @@
-﻿
+﻿function ToggleDropdown() {
+    document.querySelector(".edit-delete-modal").classList.toggle("show");
+}
+
+function CloseDropDown(event) {
+    if (!event.target.matches('.drop-btn')) {
+        var dropdowns = document.querySelectorAll(".edit-delete-modal");
+        var i;
+        for (i = 0; i < dropdowns.length; i++) {
+            var openDropdown = dropdowns[i];
+            if (openDropdown.classList.contains('show')) {
+                openDropdown.classList.remove('show');
+            }
+        }
+    }
+}
